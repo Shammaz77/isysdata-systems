5999104e5111108d315dfe8b2102c30589a3c113
shammaz77 <shammasnhd@gmail.com>
2025-11-12 12:03:56 +0530

    admin setup

---
+++ assets/js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5OAoXeNf7UGF_G3opr2jdNt31T4zPWs8",
+  authDomain: "isysdata-systems.firebaseapp.com",
+  projectId: "isysdata-systems",
+  storageBucket: "isysdata-systems.firebasestorage.app",
+  messagingSenderId: "25915958127",
+  appId: "1:25915958127:web:c136a3595bf710f05a7e9a",
+  measurementId: "G-SMYY5GYC8T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
